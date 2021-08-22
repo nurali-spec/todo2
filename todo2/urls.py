2380d8c81ce9a4cf2adbaca4c7543bc96f3d8bfd
nurali-spec <arapovnurali320@gmail.com>
2021-08-22 13:11:56 +0600

    added styles and created STATIC setting

--- todo2/urls.py
+++ todo2/urls.py
@@ -13,12 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from os import stat
 from django.contrib import admin
 from django.urls import path
 from main.views import homepage,test
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", homepage, name="home"),
     path("test/", test, name="test"),
-]
+]   +static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
+    +static(settings.MEDIA_URL , document_root=settings.MEDIA_ROOT)
