4c5da6917d7d3b84d2492d7da1b5eceb893f04d2
nurali-spec <arapovnurali320@gmail.com>
2021-08-23 10:39:18 +0600

    added method add_todo

--- main/models.py
+++ main/models.py
@@ -5,6 +5,6 @@ from django.db.models.base import Model
 
 class ToDo(models.Model):
     text = models.CharField(max_length=100)
-    created_at = models.DateField(auto_now_add=True)
-    is_closed = models.BooleanField(False)
-    is_favorite = models.BooleanField(False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_closed = models.BooleanField(default= False)
+    is_favorite = models.BooleanField(default= False)
--- main/views.py
+++ main/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 from .models import ToDo
 
 
@@ -8,3 +8,10 @@ def homepage(request):
 def test(request):
     todo_list = ToDo.objects.all()
     return render(request, "test.html",{"todo_list": todo_list})
+
+def add_todo(request):
+    form = request.POST
+    text=form["todo_text"]
+    todo=ToDo(text=text)
+    todo.save()
+    return redirect(test)
