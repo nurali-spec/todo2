6dd0809b27d3b8ab6ce3aaa9b10ed8f6ca93232d
nurali-spec <arapovnurali320@gmail.com>
2021-08-23 09:44:49 +0600

    created admin date in model panel

--- main/models.py
+++ main/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.db.models.base import Model
 
 # Create your models here.
+
+class ToDo(models.Model):
+    text = models.CharField(max_length=100)
+    created_at = models.DateField(auto_now_add=True)
+    is_closed = models.BooleanField(False)
+    is_favorite = models.BooleanField(False)
--- todo2/urls.py
+++ todo2/urls.py
@@ -24,5 +24,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("", homepage, name="home"),
     path("test/", test, name="test"),
+
+    
 ]   +static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
     +static(settings.MEDIA_URL , document_root=settings.MEDIA_ROOT)
