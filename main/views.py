59d1dca122852fee9b9d33c1ac70ff66a39b22d8
nurali-spec <arapovnurali320@gmail.com>
2021-08-23 09:57:27 +0600

    added todo list to text.html

--- main/views.py
+++ main/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render, HttpResponse
+from .models import ToDo
+
 
 def homepage(request):
     return render(request, "todo.html")
 
 def test(request):
-    return render(request, "test.html")
+    todo_list = ToDo.objects.all()
+    return render(request, "test.html",{"todo_list": todo_list})
